f88de557ad803115248d7e989a3d474fd5eee69e
CherepoGG <Sizov98@inbox.ru>
2020-05-16 14:06:56 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,118 @@
+import random
+ 
+ 
+class Hero:
+    def __init__(self, name):
+        self.name = name
+        self.max_hp = 50
+        self.hp = self.max_hp
+        self.lvl = 1
+        self.exp = 0
+        self._expForLvlUp = 30
+        self.damage = 10
+ 
+    def attack(self, enemy):
+        enemy.hp -= self.damage
+ 
+    def restore(self):
+        self.hp = self.max_hp
+ 
+    def _check_lvl_up(self):
+        if self.exp >= self._expForLvlUp:
+            self.exp -= self._expForLvlUp
+            self.lvl += 1
+            self._expForLvlUp = self._expForLvlUp * 2
+            self.max_hp += 10
+            self.hp = self.max_hp
+            self.damage += 5
+            print("Уровень повышен! Теперь ваш уровень:", self.lvl, "урон:", self.damage, "здоровье:", self.max_hp)
+ 
+    def _add_experience(self, count):
+        self.exp += count
+        print("Получено опыта:", player.exp)
+        self._check_lvl_up()
+ 
+    def reward(self, exp_reward):
+        self._add_experience(exp_reward)
+ 
+    def restore_hero(self):
+        self.hp = self.max_hp
+ 
+ 
+class Enemy:
+    def __init__(self):
+        self.name = 'Враг'
+        self.hp = 30
+        self.lvl = 1
+        self.damage = 10
+        self.exp_reward = 10
+ 
+    def attack(self, enemy):
+        enemy.hp -= self.damage
+ 
+ 
+class EnemyGenerator:
+    LIGHT = 'light'
+    MEDIUM = 'medium'
+    HARD = 'hard'
+    BOSS = 'boss'
+ 
+    def generate(self, lvl, enemy_type):
+        enemy = Enemy()
+        if enemy_type == self.LIGHT:
+            enemy.hp = 25 + (lvl * 5)
+            enemy.lvl = lvl
+            enemy.damage = 3 * lvl
+            enemy.exp_reward = 10 * lvl
+        elif enemy_type == self.MEDIUM:
+            enemy.hp = 45 + (lvl * 5)
+            enemy.lvl = lvl
+            enemy.damage = 5 * lvl
+            enemy.exp_reward = 15 * lvl
+        elif enemy_type == self.HARD:
+            enemy.hp = 65 + (lvl * 5)
+            enemy.lvl = lvl
+            enemy.damage = 7 * lvl
+            enemy.exp_reward = 30 * lvl
+        elif enemy_type == self.BOSS:
+            enemy.hp = 95 + (lvl * 5)
+            enemy.lvl = lvl
+            enemy.damage = 10 * lvl
+            enemy.exp_reward = 50 * lvl
+        return enemy
+ 
+ 
+generate_enemy = EnemyGenerator()
+player = Hero("Richard")
+ 
+ 
+def check_winner(player, enemy):
+    if enemy.hp <= 0:
+        print("Вы победили!")
+        player.reward(enemy.exp_reward)
+    elif player.hp <= 0:
+        print("Вы проиграли бой!")
+        player.restore_hero()
+        print("Воскрешение героя...")
+ 
+ 
+def battle(player, enemy):
+    current_player = random.choice((player, enemy))
+    while player.hp > 0 and enemy.hp > 0:
+        if current_player == player:
+            player.attack(enemy)
+            print(
+                player.name, 'нанес игроку', enemy.name, player.damage, 'урона. У', enemy.name, 'осталось',
+                enemy.hp, 'Здоровья')
+            current_player = enemy
+        else:
+            enemy.attack(player)
+            print(
+                enemy.name, 'нанес игроку', player.name, enemy.damage, 'урона. У', player.name, 'осталось',
+                player.hp, "/", player.max_hp, 'Здоровья')
+            current_player = player
+    check_winner(player, enemy)
+ 
+ 
+enemy = generate_enemy.generate(player.lvl, generate_enemy.LIGHT)
+battle(player, enemy)
